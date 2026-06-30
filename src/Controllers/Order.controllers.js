423ae8d9c22e339e0017d0bfa561500b448a26b5
MEETSIDHDHAPURA <meetgajjar9976@gmail.com>
2026-07-01 02:08:45 +0530

    feat: implement order management controller with CRUD operations and status tracking

--- src/Controllers/Order.controllers.js
+++ src/Controllers/Order.controllers.js
@@ -27,8 +27,8 @@ const createOrder = async (req, res) => {
       let isUnique = false;
       let newOrderId = "";
       while (!isUnique) {
-        const random = Math.floor(10000 + Math.random() * 90000);
-        newOrderId = `Order-${DD}${MM}${YY}${random}`;
+        const random = Math.floor(100000000 + Math.random() * 900000000);
+        newOrderId = `${DD}${MM}${YY}${random}`;
         const existingOrder = await Order.findOne({ orderId: newOrderId });
         if (!existingOrder) {
           isUnique = true;
